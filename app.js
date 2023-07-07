dd7d17b2334c7108a7e6caeb45a9999b1d94293a
evilebob <100439397+evilebob@users.noreply.github.com>
2023-07-07 15:40:13 -0700

    Now show me some news

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Second Edit, should auto deploy'));
+app.get('/', async (req, res) => {
+    // The URL of the GNews API endpoint
+    const url = 'https://gnews.io/api/v4/top-headlines?lang=en&country=us';
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        res.send(data);
+    } catch (error) {
+        console.log(error);
+        res.send('An error occurred while fetching the latest news.');
+    }
+});
+
+app.listen(port, () => console.log(`App running on http://localhost:${port}`));
