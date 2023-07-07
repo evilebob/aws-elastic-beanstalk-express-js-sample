f85497ab6bad6d8f946d9e0904145ae4caf72f59
evilebob <100439397+evilebob@users.noreply.github.com>
2023-07-07 15:52:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const https = require('https');
 const app = express();
 const port = 8080;
 
-app.get('/', async (req, res) => {
-    // The URL of the GNews API endpoint
-    const url = 'https://gnews.io/api/v4/top-headlines?lang=en&country=us';
+app.get('/', (req, res) => {
+    const url = 'https://api.newscatcherapi.com/v1/latest_headlines';
 
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        res.send(data);
-    } catch (error) {
-        console.log(error);
+    https.get(url, {
+        headers: {
+            'x-rapidapi-host': 'newscatcher.p.rapidapi.com',
+            'x-rapidapi-key': 'qaRM1Isa6l94Y7Gg2yLe9hxB-TOV-zFznlKxcJlziIg'
+        }
+    }, (apiRes) => {
+        let data = '';
+
+        apiRes.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        apiRes.on('end', () => {
+            res.send(JSON.parse(data));
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
         res.send('An error occurred while fetching the latest news.');
-    }
+    });
 });
 
 app.listen(port, () => console.log(`App running on http://localhost:${port}`));
